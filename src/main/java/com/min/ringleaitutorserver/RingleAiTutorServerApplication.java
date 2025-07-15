07908778478c189e27c24fe4e374224fa607ea69
Gdm0714 <ehdals45454@naver.com>
2025-07-15 14:48:10 +0900

    init: 링글 AI 튜터 프로젝트 생성

---
+++ src/main/java/com/min/ringleaitutorserver/RingleAiTutorServerApplication.java
@@ -0,0 +1,13 @@
+package com.min.ringleaitutorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingleAiTutorServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RingleAiTutorServerApplication.class, args);
+    }
+
+}
