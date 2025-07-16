dceedfd264ac0be6f2f115184000b2648f65250a
Gdm0714 <ehdals45454@naver.com>
2025-07-16 17:22:01 +0900

    feat: 커스텀 에러 추가 구현

--- src/main/java/com/min/ringleaitutorserver/exception/BusinessException.java
+++ src/main/java/com/min/ringleaitutorserver/exception/BusinessException.java
@@ -5,17 +5,20 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public class BusinessException extends RuntimeException {
+    private final ErrorCode errorCode;
     private final HttpStatus httpStatus;
     private final String code;
 
     public BusinessException(ErrorCode errorCode, String message) {
         super(message);
+        this.errorCode = errorCode;
         this.httpStatus = errorCode.getStatus();
         this.code = errorCode.getCode();
     }
 
     public BusinessException(ErrorCode errorCode) {
         super(errorCode.getCode());
+        this.errorCode = errorCode;
         this.httpStatus = errorCode.getStatus();
         this.code = errorCode.getCode();
     }
